e86bf3d87266443542e5afb1067f28ec7e9a2167
thien <s3817852@rmit.edu.vn>
2021-05-21 03:23:31 +0700

    ok

--- website/house/templatetags/search_empty.py
+++ website/house/templatetags/search_empty.py
@@ -9,3 +9,8 @@ def search_empty(empty_list, house_id):
 @register.filter
 def search_rented(rented_list, house_id):
     return rented_list.filter(house_id = house_id)
+
+@register.simple_tag
+def update_variable(value):
+    """Allows to update existing variable in template"""
+    return value
