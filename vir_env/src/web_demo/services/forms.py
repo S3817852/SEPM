628dec55c09db31940be9d663c9d70615f97c62d
thien <s3817852@rmit.edu.vn>
2022-06-14 14:16:13 +0700

    delete unnecessary files

--- vir_env/src/web_demo/services/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Announcement
-
-class AddAnnouncementForm(forms.ModelForm):
-    class Meta:
-        model = Announcement
-        fields = ['title', 'description']
-
--- vir_env/src/web_demo/user/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Userprofile, Tenant
-
-# Register your models here.
-admin.site.register(Userprofile)
-admin.site.register(Tenant)
