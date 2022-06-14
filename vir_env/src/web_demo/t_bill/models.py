ad7b206a52e245816c09f2431c5a3eb0ecbf5184
tuantng425 <tuantng425@gmail.com>
2022-06-14 15:47:22 +0700

    add table

---
+++ vir_env/src/web_demo/t_bill/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from t_account.models import Account
+from t_room.models import Room
+
+# Create your models here.
+class RentContract(models.Model):
+    account_id      = models.ForeignKey(Account, on_delete=models.CASCADE)
+    room_id         = models.ForeignKey(Room, on_delete=models.CASCADE)
+    status_Active   = models.BooleanField(default=True)
+    start_Date      = models.DateField()
+    end_Date        = models.DateField()
+    end_Date_Actual = models.DateField()
+    num_Tenants     = models.IntegerField()
+    internet_Yes    = models.BooleanField()
+    tv_Yes          = models.BooleanField()
