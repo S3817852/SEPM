0ed27bce0bad10de1098c47797f819d2e7053fbb
tuantng425 <tuantng425@gmail.com>
2022-06-14 16:07:46 +0700

    add table 4

--- vir_env/src/web_demo/t_room/models.py
+++ vir_env/src/web_demo/t_room/models.py
@@ -9,18 +9,18 @@ class Room(models.Model):
     house_id    = models.ForeignKey(House, on_delete=models.CASCADE)
     price       = models.FloatField(default=1750000) 
 
-# # class FixedFee(models.Model):
-# #     date_Update         = models.DateField(null=False)
-# #     electric_Fee        = models.FloatField(default=2200)
-# #     electric_Fee_Over   = models.FloatField(default=3000) 
-# #     water_Fee           = models.FloatField(default=10000)
-# #     water_Fee_Over      = models.FloatField(default=15000)
-# #     internet_Fee        = models.FloatField(default=50000)
-# #     tv_Fee              = models.FloatField(default=20000)
-# #     other_Fee           = models.FloatField(default=12000)  
+class FixedFee(models.Model):
+    date_Update         = models.DateField(null=False)
+    electric_Fee        = models.FloatField(default=2200)
+    electric_Fee_Over   = models.FloatField(default=3000) 
+    water_Fee           = models.FloatField(default=10000)
+    water_Fee_Over      = models.FloatField(default=15000)
+    internet_Fee        = models.FloatField(default=50000)
+    tv_Fee              = models.FloatField(default=20000)
+    other_Fee           = models.FloatField(default=12000)  
 
-# # class RoomConsumption(models.Model):
-# #     room_id         = models.ForeignKey(Room, on_delete=models.CASCADE)
-# #     month_Year      = models.DateField(null=False)
-# #     electric_Num    = models.IntegerField()
-# #     water_Num       = models.IntegerField()
+class RoomConsumption(models.Model):
+    room_id         = models.ForeignKey(Room, on_delete=models.CASCADE)
+    month_Year      = models.DateField(null=False)
+    electric_Num    = models.IntegerField()
+    water_Num       = models.IntegerField()
