9b61147a8e3c267f614fadbff555598b98175ede
tuantng425 <tuantng425@gmail.com>
2022-06-16 14:26:24 +0700

    small fix

--- vir_env/src/web_demo/t_account/models.py
+++ vir_env/src/web_demo/t_account/models.py
@@ -1,7 +1,6 @@
 from email.mime import image
 from telnetlib import STATUS
 from django.db import models
-# from t_room.models import Room
 
 # Create your models here.
 class Account(models.Model):
@@ -9,4 +8,3 @@ class Account(models.Model):
     image       = models.ImageField(null=False)
     national_ID = models.TextField(null=False)
     DoB         = models.DateField(null=False)
-
