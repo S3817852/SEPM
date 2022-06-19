e19c86287c6732773e91e75ce94a42be455d4168
PhamPhuocDong <s3817726@rmit.edu.vn>
2022-06-19 09:12:24 +0700

    consumption bugs 18/6

--- web_demo/bills/views.py
+++ web_demo/bills/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
+from .models import RoomConsumption
+
 # Create your views here.
 
 # Bills view
 def room_consumption_view(request):
-    return render(request, 'bills/roomconsumption.html', {})
+    consumption = RoomConsumption.objects.all()
+    room_consumption_info = {}
+
+
+    #Make a list of room consumption
+    for room_consumption in consumption:
+        room_consumption_info[room_consumption] = {'id':room_consumption.id, 'room':room_consumption.room_id, 'electric_consumption':room_consumption.electricity_consump,'water_consumption':room_consumption.water_consump, 'time':room_consumption.time}
+
+    context = {'consumption' : room_consumption_info}
+    if request.user.account.is_owner:
+        return render(request, 'bills/roomconsumption.html', context)
