a1acafd92e03b5fbafc28180d97de122697c75e1
Pham Phuoc Dong <s3817726@rmit.edu.vn>
2022-06-19 15:02:54 +0700

    pop up window 15:02 - 19/6

--- web_demo/bills/views.py
+++ web_demo/bills/views.py
@@ -9,11 +9,10 @@ def room_consumption_view(request):
     consumption = RoomConsumption.objects.all()
     room_consumption_info = {}
 
-
     #Make a list of room consumption
     for room_consumption in consumption:
         room_consumption_info[room_consumption] = {'id':room_consumption.id, 'room':room_consumption.room_id, 'electric_consumption':room_consumption.electricity_consump,'water_consumption':room_consumption.water_consump, 'time':room_consumption.time}
-
+    # print(room_consumption_info)
     context = {'consumption' : room_consumption_info}
     if request.user.account.is_owner:
         return render(request, 'bills/roomconsumption.html', context)
